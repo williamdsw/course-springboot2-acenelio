cc1a513952e2bc2f3ffd7913384223c6c108850c
William Santos <williamdsw@outlook.com>
2019-09-25 14:29:42 -0300

    Projeto Criado

---
+++ src/main/java/com/williamdsw/cursomodelagemconceitual/CursoModelagemConceitualApplication.java
@@ -0,0 +1,13 @@
+package com.williamdsw.cursomodelagemconceitual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoModelagemConceitualApplication 
+{
+	public static void main (String[] args) 
+	{
+		SpringApplication.run (CursoModelagemConceitualApplication.class, args);
+	}
+}
