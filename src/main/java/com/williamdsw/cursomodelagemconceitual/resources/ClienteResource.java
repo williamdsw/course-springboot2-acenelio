00099b097835a19a050e0078400d77b4ed74d067
William Santos <williamdsw@outlook.com>
2019-10-01 18:44:03 -0300

    PUT, DELETE e GET para Cliente. Criação e Bean Validation para ClienteDTO

--- src/main/java/com/williamdsw/cursomodelagemconceitual/resources/ClienteResource.java
+++ src/main/java/com/williamdsw/cursomodelagemconceitual/resources/ClienteResource.java
@@ -1,12 +1,21 @@
 package com.williamdsw.cursomodelagemconceitual.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.williamdsw.cursomodelagemconceitual.domain.Cliente;
+import com.williamdsw.cursomodelagemconceitual.dto.ClienteDTO;
 import com.williamdsw.cursomodelagemconceitual.services.ClienteService;
 
 @RestController
@@ -15,17 +24,63 @@ public class ClienteResource
 {
 	// ------------------------------------------------------------------------------------//
 	// CAMPOS
-	
+
 	@Autowired
 	private ClienteService service;
-	
+
 	// ------------------------------------------------------------------------------------//
 	// FUNCOES AUXILIARES
-	
+
+	@RequestMapping (method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll ()
+	{
+		List<Cliente> categorias = service.findAll ();
+		List<ClienteDTO> categoriasDTO = categorias.stream ().map(categoria -> new ClienteDTO (categoria)).collect (Collectors.toList ());
+		return ResponseEntity.ok ().body (categoriasDTO);
+	}
+
+	@RequestMapping (value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage (
+			@RequestParam (value = "page", defaultValue = "0") Integer pageNumber,
+			@RequestParam (value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam (value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam (value = "direction", defaultValue = "ASC") String direction)
+	{
+		Page<Cliente> categorias = service.findPage (pageNumber, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> categoriasDTO = categorias.map (categoria -> new ClienteDTO (categoria));
+		return ResponseEntity.ok ().body (categoriasDTO);
+	}
+
 	@RequestMapping (value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> findByID (@PathVariable Integer id)
 	{
 		Cliente cliente = service.findByID (id);
 		return ResponseEntity.ok ().body (cliente);
+	}
+
+	@RequestMapping (method = RequestMethod.POST)
+	public ResponseEntity<Void> insert (@Valid @RequestBody ClienteDTO categoriaDTO)
+	{
+		Cliente categoria = service.fromDTO (categoriaDTO);
+		categoria = service.insert (categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest ().path ("/{id}").buildAndExpand (categoriaDTO.getId ()).toUri ();
+		return ResponseEntity.created (uri).build ();
+	}
+
+	@RequestMapping (value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update (@Valid @RequestBody ClienteDTO categoriaDTO, @PathVariable Integer id)
+	{
+		Cliente categoria = service.fromDTO (categoriaDTO);
+		categoria.setId (id);
+		categoria = service.update (categoria);
+		return ResponseEntity.noContent ().build ();
+	}
+
+	// 1) RequestMethod.DELETE = Indica exclusao de dados
+	@RequestMapping (value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> deleteByID (@PathVariable Integer id)
+	{
+		service.deleteByID (id);
+		return ResponseEntity.noContent ().build ();
 	}
 }
--- src/main/java/com/williamdsw/cursomodelagemconceitual/services/ClienteService.java
+++ src/main/java/com/williamdsw/cursomodelagemconceitual/services/ClienteService.java
@@ -1,10 +1,17 @@
 package com.williamdsw.cursomodelagemconceitual.services;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import com.williamdsw.cursomodelagemconceitual.domain.Cliente;
+import com.williamdsw.cursomodelagemconceitual.dto.ClienteDTO;
 import com.williamdsw.cursomodelagemconceitual.repositories.ClienteRepository;
+import com.williamdsw.cursomodelagemconceitual.services.exceptions.DataIntegrityException;
 import com.williamdsw.cursomodelagemconceitual.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -12,16 +19,73 @@ public class ClienteService
 {
 	// ------------------------------------------------------------------------------------//
 	// CAMPOS
-	
+
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	// ------------------------------------------------------------------------------------//
 	// FUNCOES AUXILIARES
-	
+
+	// Busca todos
+	public List<Cliente> findAll ()
+	{ 
+		return repository.findAll (); 
+	}
+
+	// Busca por ID
 	public Cliente findByID (Integer id)
 	{
 		Optional<Cliente> cliente = repository.findById (id);
-		return cliente.orElseThrow (() -> new ObjectNotFoundException (" Objeto não encontrado! " + " Id: " + id + " Tipo: " + Cliente.class.getName ()));
+		return cliente.orElseThrow ( () -> new ObjectNotFoundException (" Objeto não encontrado! " + " Id: " + id + " Tipo: " + Cliente.class.getName ()));
+	}
+
+	// Busca com paginacao
+	public Page<Cliente> findPage (Integer pageNumber, Integer linesPerPage, String orderBy, String direction)
+	{
+		PageRequest pageRequest = PageRequest.of (pageNumber, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repository.findAll (pageRequest);
+	}
+
+	// Insere
+	public Cliente insert (Cliente cliente)
+	{
+		cliente.setId (null);
+		return repository.save (cliente);
+	}
+
+	// Atualiza
+	public Cliente update (Cliente cliente)
+	{
+		Cliente novoCliente = findByID (cliente.getId());
+		updateData (novoCliente, cliente);
+		return repository.save (novoCliente);
+	}
+
+	// Exclui por ID
+	public void deleteByID (Integer id)
+	{
+		findByID (id);
+
+		try
+		{
+			repository.deleteById (id);
+		}
+		catch (DataIntegrityViolationException exception)
+		{
+			throw new DataIntegrityException ("Não é possível excluir um Cliente!");
+		}
+	}
+
+	// Converte dados
+	public Cliente fromDTO (ClienteDTO dto)
+	{
+		return new Cliente (dto.getId (), dto.getNome (), dto.getEmail (), null, null);
+	}
+	
+	// Atualiza dados
+	private void updateData (Cliente novoCliente, Cliente cliente)
+	{
+		novoCliente.setNome (cliente.getNome ());
+		novoCliente.setEmail (cliente.getEmail ());
 	}
 }
