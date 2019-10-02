896235f3b79876a97cc575c30d4bcf43664d55e7
William Santos <williamdsw@outlook.com>
2019-10-02 14:08:30 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/williamdsw/cursomodelagemconceitual/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/williamdsw/cursomodelagemconceitual/services/validation/ClienteInsertValidator.java
@@ -4,13 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.williamdsw.cursomodelagemconceitual.domain.Cliente;
 import com.williamdsw.cursomodelagemconceitual.domain.enums.TipoCliente;
 import com.williamdsw.cursomodelagemconceitual.dto.ClienteNewDTO;
+import com.williamdsw.cursomodelagemconceitual.repositories.ClienteRepository;
 import com.williamdsw.cursomodelagemconceitual.resources.exceptions.FieldMessage;
 import com.williamdsw.cursomodelagemconceitual.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO>
 {
+	// ------------------------------------------------------------------------------------//
+	// CAMPOS
+	
+	@Autowired
+	private ClienteRepository repository;
+	
+	// ------------------------------------------------------------------------------------//
+	// IMPLEMENTADOS
+	
 	@Override
 	public void initialize (ClienteInsert ann) { }
 
@@ -19,17 +31,24 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	{ 
 		List<FieldMessage> errors = new ArrayList<>();
 		
+		// Verifica CPF
 		if (dto.getTipoCliente ().equals (TipoCliente.PESSOA_FISICA.getCodigo ()) && !BR.isValidCPF (dto.getCpfOuCnpj ()))
 		{
 			errors.add (new FieldMessage ("cpfOuCnpj", "CPF inválido!"));
 		}
 		
+		// Verifica CNPJ
 		if (dto.getTipoCliente ().equals (TipoCliente.PESSOA_JURIDICA.getCodigo ()) && !BR.isValidCNPJ (dto.getCpfOuCnpj ())) 
 		{
 			errors.add (new FieldMessage ("cpfOuCnpj", "CNPJ inválido!"));
 		}
 		
-		// inclua os testes aqui, inserindo erros na lista
+		// Verifica EMAIL
+		Cliente cliente = repository.findByEmail (dto.getEmail ());
+		if (cliente != null)
+		{
+			errors.add (new FieldMessage ("email", "Email já existente"));
+		}
 		
 		for (FieldMessage error : errors)
 		{
