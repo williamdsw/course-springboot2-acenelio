b30de9d129b860ca9b0099c8215d325b55e7b3a0
William Santos <williamdsw@outlook.com>
2019-10-08 17:35:59 -0300

    Configuração inicial do Spring Security. Adicionado propriedades de email para o application-test.properties para não ocorrer lançar erro de Bean de email

--- src/main/java/com/williamdsw/cursomodelagemconceitual/services/PedidoService.java
+++ src/main/java/com/williamdsw/cursomodelagemconceitual/services/PedidoService.java
@@ -87,8 +87,8 @@ public class PedidoService
         
         itemPedidoRepository.saveAll (pedido.getItens ());
         System.out.println (pedido);
-        //emailService.sendOrderConfirmationEmail (pedido);
-        emailService.sendOrderConfirmationHtmlEmail (pedido);
+        emailService.sendOrderConfirmationEmail (pedido);
+        //emailService.sendOrderConfirmationHtmlEmail (pedido);
         
         return pedido;
     }
